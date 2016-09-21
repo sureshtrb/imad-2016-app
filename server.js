d854dd098a89fe144ecaffbe07650ad309868e52
sureshtrb <sureshtrb@gmail.com>
2016-09-21 21:25:07 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'Article One | Suresh',
+    heading: 'Article One',
+    date: 'September 20, 2016',
+    content:`
+    <p>
+        This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page.
+    </p>
+    <p>
+        This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page.
+     </p>
+        <p>
+        This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page.
+    </p>`
+};
+var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                 <a href="/">Home</a>
+            </div>
+            <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+            <div>
+                ${date}
+            </div>
+             <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>`
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
