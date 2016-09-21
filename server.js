aee3cea44a7a273a1e761d4233ecb19c9e8a390c
sureshtrb <sureshtrb@gmail.com>
2016-09-21 21:31:49 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ var content = {
         This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page. This is my content on the Article One page.
     </p>`
 };
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+
 var htmlTemplate = `
     <html>
     <head>
@@ -47,15 +53,16 @@ var htmlTemplate = `
             </div>
         </div>
     </body>
-</html>`
-
-
+</html>
+`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
