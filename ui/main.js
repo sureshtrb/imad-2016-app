2ee2b68f59d194515464b07e8b104d029ffd64f9
sureshtrb <sureshtrb@gmail.com>
2016-09-28 22:01:05 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 // change the document balue text
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value"
-var img = document.getElementById("imahe");
+var img = document.getElementById("madi");
 img.onclick = function(){
   img.style.marginLeft = '100px';  
 };
