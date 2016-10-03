350c74886598f2ac68c2737612668cfb01c713bb
sureshtrb <sureshtrb@gmail.com>
2016-10-03 20:54:13 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,29 @@ button.onclick = function() {
         request.open('GET', 'http://sureshtrb.imad.hasura-app.io/counter', true);
         request.send(null);
 };
+
+//Submit Name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i < names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ui = document.getElementById('namelist');
+                ul.innerHTML = list;
+                
+            }
+        }
+    };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://sureshtrb.imad.hasura-app.io/counter', true);
+        request.send(null);
+};
