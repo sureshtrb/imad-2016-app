963b8e6bfe74453845171ae21beef4dc360e01bf
sureshtrb <sureshtrb@gmail.com>
2016-10-03 14:41:09 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,9 @@
-console.log('Loaded!');
-// change the document balue text
-var element = document.getElementById("main-text");
-element.innerHTML = "New Value";
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-  var interval = setInterval(moveRight, 100); 
+//counter code
+var button = document.getElementById("counter");
+var counter = 0;
+
+button.onClick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString;
 };
