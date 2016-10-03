a7398fed3ecdd8e3a4ad94604be640f374740c3c
sureshtrb <sureshtrb@gmail.com>
2016-10-03 14:42:45 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ var counter = 0;
 button.onClick = function() {
     counter = counter + 1;
     var span = document.getElementById("count");
-    span.innerHTML = counter.toString;
+    span.innerHTML = counter.toString();
 };
