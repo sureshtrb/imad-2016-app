e7269129a8d27397c3359d5e8d7a0f8e5e06c202
sureshtrb <sureshtrb@gmail.com>
2016-10-04 12:22:24 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,18 +93,14 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
+//counter
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
-var names = [];
-app.get('/submit-name: name',function(req, res) {
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
