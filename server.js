f659e791e12fa016501db8ae2f0ed92dcb0fb8ec
sureshtrb <sureshtrb@gmail.com>
2016-10-06 17:00:56 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,12 @@ app.get('/favicon.ico', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+app.get('/submit-name/:name', function(req, res){
+    var name = req.params.name;
+    name.push(name);
+    //JASON
+    res.send(JSON.stringify(names));
+})
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
