4d699fe7d88d62168ea3709aa56f05fb1d8c8b8a
sureshtrb <sureshtrb@gmail.com>
2016-10-08 12:25:02 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,19 @@ button.onclick = function() {
     request.open('GET', 'http://sureshtrb.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // make a request to the server and send the name
+    
+    //capture the list of name and render in a list
+    var names = ['name1','name2','name3', 'name4'];
+    var list ='';
+    for(var i = 0; i < names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
