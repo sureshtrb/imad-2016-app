533f794b75a413c032460b7bd95b7e8094d0daa8
sureshtrb <sureshtrb@gmail.com>
2016-10-08 12:11:49 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-    //make a request to the counter endpoint
+    //create  a request to the counter endpoint
+    var request = new XMLHttpRequest();
     
     //Capture the request and store it in a variable
-  //Render the variable 
-  counter = counter +1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    //make the request
+    request.open('GET', 'http://sureshtrb.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
